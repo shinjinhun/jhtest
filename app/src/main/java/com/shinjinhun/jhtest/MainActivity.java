03fc9965cc3d42cfd62f2f0c61b1347ad7c10eca
shinjinhun <mamsorea@gmail.com>
2019-06-11 10:50:49 +0900

    홍드로이드 안드로이드 실습

---
+++ app/src/main/java/com/shinjinhun/jhtest/MainActivity.java
@@ -0,0 +1,43 @@
+package com.shinjinhun.jhtest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move;
+    private EditText et_test;
+    private String str;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_test = findViewById(R.id.editText01);
+
+        btn_move = findViewById(R.id.button01);
+
+        btn_move.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                str = et_test.getText().toString();
+
+                Toast.makeText(getApplicationContext(),"메인 버튼 클릭 : " + str  , Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str",str);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/shinjinhun/jhtest/SubActivity.java
@@ -0,0 +1,50 @@
+package com.shinjinhun.jhtest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SubActivity extends AppCompatActivity {
+
+    private TextView textView02;
+    private Button button02;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        textView02 = findViewById(R.id.textView02);
+
+        button02 = findViewById(R.id.button02);
+
+        Intent  intent = getIntent();
+        String str = intent.getStringExtra("str");
+
+        Log.d("jhTEST",str);
+
+        Toast.makeText(getApplicationContext(),"버튼 클릭: " + str  , Toast.LENGTH_LONG).show();
+
+        textView02.setText(str);
+
+        button02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"메인으로 돌아가기"   , Toast.LENGTH_LONG).show();
+
+
+                Intent intent = new Intent(SubActivity.this, MainActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+    }
+}
