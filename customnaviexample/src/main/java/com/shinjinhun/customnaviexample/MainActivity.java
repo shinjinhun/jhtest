e9f8f57ab32130a26d2e4b5f7f4926dc9e645825
shinjinhun <mamsorea@gmail.com>
2019-06-12 09:41:48 +0900

    홍드로이드 안드로이드 실습

---
+++ customnaviexample/src/main/java/com/shinjinhun/customnaviexample/MainActivity.java
@@ -0,0 +1,74 @@
+package com.shinjinhun.customnaviexample;
+
+import android.support.annotation.NonNull;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView  = (View)findViewById(R.id.drawer);
+
+
+        Button btn_open = (Button)findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+        Button btn_close = (Button)findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+
+    }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View view, float v) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View view) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View view) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int i) {
+
+        }
+    };
+
+}
