f26e7d67f7952c09de32fc3beef47fb13dfea3f7
shinjinhun <mamsorea@gmail.com>
2019-06-14 01:04:51 +0900

    리사이클 예제 작업중....

---
+++ recyclerviewexample/src/main/java/com/example/recyclerviewexample/MainAdapter.java
@@ -0,0 +1,53 @@
+package com.example.recyclerviewexample;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.CustomViewHolder> {
+
+    private ArrayList<MainData> arrayList;
+
+    public MainAdapter(ArrayList<MainData> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public MainAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,parent, false);
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainAdapter.CustomViewHolder customViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        protected ImageView iv_profile;
+        protected TextView tv_name;
+        protected TextView tv_content;
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.iv_profile = (ImageView) itemView.findViewById(R.id.iv_profile);
+            this.tv_name = (TextView) itemView.findViewById(R.id.tv_name);
+            this.tv_content = (TextView) itemView.findViewById(R.id.tv_content);
+        }
+    }
+}
