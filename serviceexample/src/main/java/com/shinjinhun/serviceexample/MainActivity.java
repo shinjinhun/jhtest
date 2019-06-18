1ef56bcd547e34788a87cc783bcc946007cad42a
shinjinhun <mamsorea@gmail.com>
2019-06-18 09:34:09 +0900

    Service 백그라운드 음악 재생

---
+++ serviceexample/src/main/java/com/shinjinhun/serviceexample/MainActivity.java
@@ -0,0 +1,40 @@
+package com.shinjinhun.serviceexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+// Service 백그라운드 음악 재생
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_start, btn_stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 서비스 시작
+        btn_start = (Button)findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(getApplicationContext(), MusicService.class));
+            }
+        });
+
+        // 서비스 종료
+        btn_stop = (Button)findViewById(R.id.btn_stop);
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(getApplicationContext(), MusicService.class));
+            }
+        });
+
+
+
+    }
+}
