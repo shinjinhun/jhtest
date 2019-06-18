7cb786200d33f9e8c9ad61cf18499c844958397e
shinjinhun <mamsorea@gmail.com>
2019-06-18 21:49:06 +0900

    // #21 (로딩화면 만들기 feat. github) // https://github.com/ybq/Android-SpinKit

---
+++ loadinganimationexample/src/main/java/com/example/loadinganimationexample/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.loadinganimationexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.ybq.android.spinkit.style.DoubleBounce;
+
+// #21 (로딩화면 만들기 feat. github)
+// https://github.com/ybq/Android-SpinKit
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
