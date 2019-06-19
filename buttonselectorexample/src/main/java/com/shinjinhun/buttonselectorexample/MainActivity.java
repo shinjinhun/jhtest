820c4320fdaa4a354f43d7031a2d0b8d6cab7f86
shinjinhun <mamsorea@gmail.com>
2019-06-19 16:47:28 +0900

    #27 Selector ( 버튼, 이미지 눌렀을때 효과주기 )

---
+++ buttonselectorexample/src/main/java/com/shinjinhun/buttonselectorexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shinjinhun.buttonselectorexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// #27 Selector ( 버튼, 이미지 눌렀을때 효과주기 )
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
